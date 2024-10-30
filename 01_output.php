a59e739f4879f783beb79b3fc000c219e7a3cd2f
Andrés Tanesini <andres.tanesini94@gmail.com>
2024-10-30 08:41:23 +0100

    feat: output basics in PHP

--- 01_output.php
+++ 01_output.php
@@ -1,3 +1,13 @@
 <?php
-    echo 'Hello';
-?>
+
+// echo can take multiple values and displaying them
+echo 'Hello', 2;
+
+// print_r() can only take one argument
+print_r([1, 2, 3]);
+
+/* 
+    var_dump() -> useful function that returns data type,
+    lenght and display value
+*/
+var_dump([1, 2, 3]);
