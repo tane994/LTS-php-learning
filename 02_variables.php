b2fd058367f10e5e5dba62bc4dc2944a4bca66c4
Andrés Tanesini <andres.tanesini94@gmail.com>
2024-10-30 09:37:23 +0100

    feat: learn how to define variables, constants and use data types

---
+++ 02_variables.php
@@ -0,0 +1,16 @@
+<?php
+
+$name = "Andrés";
+$age = 30;
+$has_job = true;
+$cash_on_hand = 25.50;
+
+echo $name . " is" . $age . " years old.";
+
+// var_dump($has_job);
+
+// Constants
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+// echo HOST;
